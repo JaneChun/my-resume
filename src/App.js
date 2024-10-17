37eea575428c1f71a98ae01fca6ecacffa474afe
JaneChun <janechun22@gmail.com>
2024-10-17 22:57:29 +0900

    전역 스타일 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import styled from 'styled-components';
 import Header from './components/Header';
 import MainSection from './components/MainSection';
 import Footer from './components/Footer';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-size: 16px;
+  }
+`;
 
 function App() {
 	return (
-		<PageWrapper>
-			<Header />
-			<MainSection />
-			<Footer />
-		</PageWrapper>
+		<>
+			<GlobalStyle />
+			<PageWrapper>
+				<Header />
+				<MainSection />
+				<Footer />
+			</PageWrapper>
+		</>
 	);
 }
 
@@ -18,7 +33,6 @@ const PageWrapper = styled.div`
 	font-family: 'Arial', sans-serif;
 	color: #333;
 	line-height: 1.6;
-	padding: 40px 20px;
 	margin: 0 auto;
 	max-width: 50rem;
 	padding: 1rem 2rem 3rem 2rem;
